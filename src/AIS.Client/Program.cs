f215576e26af1fb90a21135eb30e0194783ef12f
Jan Marius <jan.marius@bitbucket.no>
2019-09-01 15:52:23 +0200

    Added test client using ais data from kystverket

---
+++ src/AIS.Client/Program.cs
@@ -0,0 +1,47 @@
+﻿using Bitbucket.AIS;
+using Bitbucket.AIS.Messages;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Sockets;
+
+namespace AIS.Client
+{
+    public static class Program
+    {
+        private static void Run()
+        {
+            var aisDecoder = new AisDecoder();
+
+            TcpClient client = new TcpClient("153.44.253.27", 5631);
+            NetworkStream networkStream = client.GetStream();
+            StreamReader reader = new StreamReader(networkStream);
+
+            while (true)
+            {
+                var nmeaList = new List<NmeaMessage>();
+                string nmeaString = reader.ReadLine();
+                var nmeaMsg = aisDecoder.GetNmeaMessage(nmeaString);
+
+                nmeaList.Add(nmeaMsg);
+
+                while (nmeaMsg.NumberOfSentences > 1 && nmeaMsg.SentenceNumber != nmeaMsg.NumberOfSentences)
+                {
+                    nmeaString = reader.ReadLine();
+                    nmeaMsg = aisDecoder.GetNmeaMessage(nmeaString);
+                    nmeaList.Add(nmeaMsg);
+                }
+
+                var msg = aisDecoder.GetAisMessage(nmeaList);
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine(msg.ToString());
+            }
+        }
+    
+
+        static void Main(string[] args)
+        {
+            Run();
+        }
+    }
+}
